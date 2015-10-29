8fb8d61b9e2ea850424333c8b7278a8371c5e431
Leonardo Velarde <leonardo.velarde.ldvc@gmail.com>
2015-10-29 08:17:01 -0400

    omaiga

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,5 @@
 class Client < ActiveRecord::Base
-  validates :name, presence: {:message => 'You must enter a name'}
+  validates :name, presence: {:message => 'You must enter a name other stuff'}
   validates :nit, presence: {:message => 'You must enter clients nit'}
   validates :nit, numericality: {message: 'Only  numbers' }
 end
